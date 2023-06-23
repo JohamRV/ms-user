1c7ddeb038853d1e822edb336577ab19173fec15
Joham Romucho <joham.romucho@pucp.edu.pe>
2023-06-23 17:25:22 -0500

    feat: set up basic microservice

---
+++ src/main/java/pe/edu/pucp/msuser/controller/UserController.java
@@ -0,0 +1,4 @@
+package pe.edu.pucp.msuser.controller;
+
+public class UserController {
+}
---
+++ src/test/java/pe/edu/pucp/msuser/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.msuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
